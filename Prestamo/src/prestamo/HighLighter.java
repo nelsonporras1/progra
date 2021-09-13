edf576db2200c60fc44830836182d525b4b84679
Elias29-6 <80367751+Elias29-6@users.noreply.github.com>
2021-09-12 19:46:01 -0600

    v0.1

---
+++ Prestamo/src/prestamo/HighLighter.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prestamo;
+
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.ImageIcon;
+/**
+ *
+ * @author 50663
+ */
+/*public class HighLighter extends MouseAdapter implements MouseListener {
+    
+    
+    public void mouseEntered(MouseEvent evt){
+        
+      Component source= (Component) evt.getSource();
+      
+   
+      if( evt.getX() > 24 && evt.getX() > 176  && evt.getY() > 100 && evt.getY() < 169){
+      source.setIcon();
+              
+      }
+      
+    }
+    
+    
+}
+*/
